41cdc6f6fc1c3e8bf95e84cd12874093bdb0bc16
soph28 <sophia.ehmayer@gmail.com>
2024-03-27 18:53:56 +0100

    Create project

---
+++ src/main/java/com/example/tourplanner/TourplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourplannerApplication.class, args);
+	}
+
+}
