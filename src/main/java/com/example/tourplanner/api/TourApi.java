7c36fd7700f2534407a71ca5379cafeb4101c4da
soph28 <sophia.ehmayer@gmail.com>
2024-03-29 11:27:14 +0100

    implement createTour

--- src/main/java/com/example/tourplanner/api/TourApi.java
+++ src/main/java/com/example/tourplanner/api/TourApi.java
@@ -1,4 +1,27 @@
 package com.example.tourplanner.api;
 
+
+import com.example.tourplanner.service.TourService;
+import com.example.tourplanner.service.dtos.TourDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "tour")
+
+
 public class TourApi {
+
+    @Autowired
+    private TourService tourService;
+
+    @PostMapping
+    public ResponseEntity<TourDto> createTour(@RequestBody TourDto tourDto) {
+        tourService.createTour(tourDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
