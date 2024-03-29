939949f57407369c36285bbb47f245b8573d893c
soph28 <sophia.ehmayer@gmail.com>
2024-03-29 11:24:54 +0100

    implement dtos

--- src/main/java/com/example/tourplanner/service/dtos/TourDto.java
+++ src/main/java/com/example/tourplanner/service/dtos/TourDto.java
@@ -1,4 +1,22 @@
 package com.example.tourplanner.service.dtos;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class TourDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String arrival;
+    private String departure;
+    private String transportType;
+    private Double distance;
+    private String estimatedTime;
+    private String routeImage;
 }
--- src/main/java/com/example/tourplanner/service/dtos/TourLogDto.java
+++ src/main/java/com/example/tourplanner/service/dtos/TourLogDto.java
@@ -1,4 +1,25 @@
 package com.example.tourplanner.service.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class TourLogDto {
+
+    private Long id;
+    private Long tourId; //refers to the associated tour
+    private LocalDate date;
+    private String comment;
+    private Integer difficulty;
+    private Double totalDistance;
+    private String totalTime;
+    private Integer rating;
 }
