3286019a400436d67892a05c7f492dffbc27053e
soph28 <sophia.ehmayer@gmail.com>
2024-03-29 11:25:57 +0100

    implement entities

--- src/main/java/com/example/tourplanner/persistence/entities/TourEntity.java
+++ src/main/java/com/example/tourplanner/persistence/entities/TourEntity.java
@@ -1,4 +1,41 @@
 package com.example.tourplanner.persistence.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tour")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class TourEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private String departure;
+
+    @Column(nullable = false)
+    private String arrival;
+
+    @Column(nullable = false)
+    private String transportType;
+
+    @Column(nullable = false)
+    private Double distance;
+
+    @Column(name = "estimated_time")
+    private String estimatedTime;
+
+    @Column(name = "route_image")
+    private String routeImage;
 }
--- src/main/java/com/example/tourplanner/persistence/entities/TourLogEntity.java
+++ src/main/java/com/example/tourplanner/persistence/entities/TourLogEntity.java
@@ -1,4 +1,41 @@
 package com.example.tourplanner.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tour_log")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class TourLogEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne //one tour has many tourLogs,
+    @JoinColumn(name = "tour_id", nullable = false)
+    private TourEntity tour;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(length = 500)
+    private String comment;
+
+    @Column(nullable = false)
+    private Integer difficulty;
+
+    @Column(name = "total_distance", nullable = false)
+    private Double totalDistance;
+
+    @Column(name = "total_time", nullable = false)
+    private String totalTime;
+
+    @Column(nullable = false)
+    private Integer rating;
 }
