58976045954d7d164f025fc1d9af58b7a12e47f2
soph28 <sophia.ehmayer@gmail.com>
2024-03-30 11:31:11 +0100

    implement tourLogMapper

--- src/main/java/com/example/tourplanner/service/mapper/TourLogMapper.java
+++ src/main/java/com/example/tourplanner/service/mapper/TourLogMapper.java
@@ -1,4 +1,24 @@
 package com.example.tourplanner.service.mapper;
 
-public class TourLogMapper {
+import com.example.tourplanner.persistence.entities.TourEntity;
+import com.example.tourplanner.persistence.entities.TourLogEntity;
+import com.example.tourplanner.service.dtos.TourDto;
+import com.example.tourplanner.service.dtos.TourLogDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TourLogMapper extends AbstractMapper<TourLogEntity, TourLogDto> {
+    @Override
+    public TourLogDto mapToDto(TourLogEntity source) {
+        return TourLogDto.builder()
+                .id(source.getId())
+                .tourId(source.getTour().getId())
+                .date(source.getDate())
+                .comment(source.getComment())
+                .difficulty(source.getDifficulty())
+                .totalDistance(source.getTotalDistance())
+                .totalTime(source.getTotalTime())
+                .rating(source.getRating())
+                .build();
+    }
 }
